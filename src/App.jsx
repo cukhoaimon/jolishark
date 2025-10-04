fd2301b3100064f9aa27cfe44a90019cc1e1e3b6
vikki-phucnguyen <phuc.nguyen@vikki.vn>
2025-10-04 10:30:22 +0700

    git

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Material UI Vite.js example
+          Jolibeeeeeeeeeeeeeeeeeeee
         </Typography>
         <ProTip />
         <Copyright />
