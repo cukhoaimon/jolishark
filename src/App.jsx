b0a4c73f6e824e3d1e0d6eb9ab83af9e193cce95
vikki-phucnguyen <phuc.nguyen@vikki.vn>
2025-10-04 10:43:01 +0700

    view

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,22 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import ProTip from './ProTip';
 import Copyright from './Copyright';
+import {Viewer} from "resium";
 
 export default function App() {
-  return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Jolibeeeeeeeeeeeeeeeeeeee
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-  );
+    return (
+        <Container maxWidth="sm">
+            <Box sx={{my: 4}}>
+                <Typography variant="h4" component="h1" sx={{mb: 2}}>
+                    Jolibeeeeeeeeeeeeeeeeeeee
+                </Typography>
+
+
+                <ProTip/>
+                <Copyright/>
+
+                <Viewer full/>
+            </Box>
+        </Container>
+    );
 }
